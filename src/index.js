cb2d77c5a96a50aae91f3f1412338cbd854cfdf2
Giedrius <giedrius.vickus@gmail.com>
2021-02-13 17:41:19 +0200

    Initial commit

--- src/index.js
+++ src/index.js
@@ -1,9 +1,26 @@
-import React, {Component} from 'react'
+import React from "react";
+import { Link } from "react-router-dom";
 
-export default class extends Component {
-  render() {
-    return <div>
-      <h2>Welcome to React components</h2>
-    </div>
-  }
-}
+const GenericLink = ({ to, children, ...props }) => {
+  // It is a simple element with nothing to link to
+  if (!to) return <button {...props}>{children}</button>;
+
+  const isExternalLink = /^https?:\/\//.test(to);
+
+  // It is intended to be an external link
+  if (isExternalLink)
+    return (
+        <a href={to} {...props}>
+          {children}
+        </a>
+    );
+
+  // it is an internal link
+  return (
+      <Link to={to} {...props}>
+        {children}
+      </Link>
+  );
+};
+
+export default GenericLink;
