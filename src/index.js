1c1ff5385425f3a67726b29af27459c1111904d2
Giedrius <giedrius.vickus@gmail.com>
2021-02-26 14:51:43 +0200

    Added support fot "target"
    Target property will be rendered only for external link output.

--- src/index.js
+++ src/index.js
@@ -2,14 +2,16 @@ import React from "react";
 import { Link } from "react-router-dom";
 
 /**
+ *
  * @param to External or internal URL
  * @param children
  * @param type Button type, if URL is not provided
+ * @param target
  * @param props
  * @returns {*}
  * @constructor
  */
-const ReactSmartLink = ({ to, children, type, ...props }) => {
+const ReactSmartLink = ({ to, children, type, target, ...props }) => {
   // It is a simple element with nothing to link to
   if (!to) {
       if (type) {
@@ -24,7 +26,7 @@ const ReactSmartLink = ({ to, children, type, ...props }) => {
   // It is intended to be an external link
   if (isExternalLink)
     return (
-        <a href={to} {...props}>
+        <a href={to} target={target} {...props}>
           {children}
         </a>
     );
