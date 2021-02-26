489990465389de76f8d0ce54bfe1f4b0a77c530d
Giedrius <giedrius.vickus@gmail.com>
2021-02-26 14:33:12 +0200

    Added support for SPAN element.
    If {to} and {type} is not provided, component will fallback to <span>

--- src/index.js
+++ src/index.js
@@ -4,14 +4,19 @@ import { Link } from "react-router-dom";
 /**
  * @param to External or internal URL
  * @param children
+ * @param type Button type, if URL is not provided
  * @param props
  * @returns {*}
  * @constructor
  */
-const ReactSmartLink = ({ to, children, ...props }) => {
+const ReactSmartLink = ({ to, children, type, ...props }) => {
   // It is a simple element with nothing to link to
   if (!to) {
-      return <button type="button" {...props}>{children}</button>;
+      if (type) {
+          return <button type={type} {...props}>{children}</button>;
+      } else {
+          return <span {...props}>{children}</span>;
+      }
   }
 
   const isExternalLink = /^https?:\/\//.test(to);
